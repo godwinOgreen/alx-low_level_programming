c932f81cc04062dc565cf8e51c40c3d405000035
godwin <godwinemerald288@gmail.com>
2023-03-16 21:36:29 -0700

    Update Scripts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+        printf("SIze of a char: %d byte(s)\n", sizeof(char));
+	printf("SIze of an int: %d byte(s)\n", sizeof(int));
+	printf("SIze of a long int:%d byte(s)\n", sizeof(long int));
+	printf("SIze of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("SIze of a float: %d byte(s)\n", sizeof(float));
+        return(0);
+}               
+
